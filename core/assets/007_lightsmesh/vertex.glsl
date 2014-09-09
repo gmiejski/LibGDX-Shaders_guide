8303c0763c7e4d9d57dbedd3e6d58b388d331cb1
Mateusz Antkiewicz <antkiewicz.mateusz@gmail.com>
2014-09-09 21:35:53 +0200

    Fix diffuse

--- core/assets/007_lightsmesh/vertex.glsl
+++ core/assets/007_lightsmesh/vertex.glsl
@@ -11,8 +11,8 @@ varying vec3 v_Normal;
 
 
 void main() {
-    v_Position = a_position;
-    v_Normal = a_normal;
+    v_Position = vec3(u_rotationMatrix * vec4(a_position, 0.0f));
+    v_Normal = vec3(u_rotationMatrix * vec4(a_normal, 0.0f));
     v_Color = vec4(a_color.xyz,0);
 
     gl_Position = u_rotationMatrix * vec4(a_position.xyz, 1.0f);
